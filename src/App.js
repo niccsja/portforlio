786dd1ae3be4fe6f28c9bd0fbe82950657018a71
Niville Nicholas <niville.nicc@gmail.com>
2022-01-20 13:45:18 -0500

    application architecture setup/ linter added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+
+const App = () => {
+	return (
+		<div>
+			<h1> My Porforlio project</h1>
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/headers/HeaderNav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderNav = () => {
+	return (
+		<>
+			<nav>
+				<ul></ul>
+			</nav>
+		</>
+	)
+}
+
+export default HeaderNav
